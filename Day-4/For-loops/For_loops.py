59de395ab42d391004b09eac2ff5054212150732
chaudharyakshat239 <chaudharyakshat239@gmail.com>
2025-12-08 17:15:34 +0530

    "For Loops"

--- Day-4/For-loops/For_loops.py
+++ Day-4/For-loops/For_loops.py
@@ -3,20 +3,20 @@
 
 
 # printing numbers from 10 to 40
-# for number in range(10,41,1):   # "i" is used for iteration over elements
-#     print(akku)
+for number in range(10,41,1):   # "i" is used for iteration over elements
+    print(akku)
 
 # printing numbers from -10 to 20
-# for num in range(-10,21,1):
-#     print(num)
+for num in range(-10,21,1):
+    print(num)
 
 # printing number from 34 to 5
-# for num1 in range(34,5,-1):
-#     print(num1)
+for num1 in range(34,5,-1):
+    print(num1)
 
 # printing my name 10 time with count
-# for name in range(1,11,1):
-#     print("Akku",name)
+for name in range(1,11,1):
+    print("Akku",name)
 
 # printing the table of 5
 for table in range(5,51,5):
