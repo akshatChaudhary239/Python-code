52683fc74d839a4ab068983c2bcabe4c87a195d3
chaudharyakshat239 <chaudharyakshat239@gmail.com>
2025-12-18 20:13:19 +0530

    "Lists"

---
+++ Day-6/Lists/Lists.py
@@ -0,0 +1,20 @@
+# Data structures 
+# data  structures are sued to store mulit[le values in a single varibale
+# lists , tuples , sets , dictionaries are the most commonly used data structures in python
+
+# List
+a = [1,2,4.5,"akshat"] 
+print(a)
+
+# List are mutuable
+b = [3,4,5,6]
+b[2]= 7  #list indexing
+print(b)
+
+# List's slicing and indexing are same as string's
+c = [6,7,9,3,5]
+print(c[:2]) #list slicing
+
+# we can store duplicate values in a list
+d = [ 1,5,6,1,5,6,1,5,6,1,5,6,1,5,6]
+print(d)
