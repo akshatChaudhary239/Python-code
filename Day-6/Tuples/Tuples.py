7d61891ac9d6d928accd0f225cc15545f8b7dd82
chaudharyakshat239 <chaudharyakshat239@gmail.com>
2025-12-22 17:17:14 +0530

    "tuples"

---
+++ Day-6/Tuples/Tuples.py
@@ -0,0 +1,7 @@
+# Tuples are also used to store mulyiple values in a single variable
+# We cannot change elements in a tuple means it is immutable
+
+# the only differences in list and tuple is of brackets and immutabilty
+
+a = (10,20,30,40,50)
+print(len(a))
