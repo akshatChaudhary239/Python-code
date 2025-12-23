f7efb85ec19ad4803215dca41eb7e9a941f24f58
chaudharyakshat239 <chaudharyakshat239@gmail.com>
2025-12-23 16:01:53 +0530

    "dictionary"

---
+++ Day-6/Dictionary/Dictionary.py
@@ -0,0 +1,8 @@
+# Dictionary  - it is also a container in which we can store multiple data types
+# we store values on dictionary in keys and values forms
+# Values are mutable but keys are not
+d = {10:100,
+     20:200,
+     30:300}
+
+print(d[20]) #wew access values like this in dictionary
