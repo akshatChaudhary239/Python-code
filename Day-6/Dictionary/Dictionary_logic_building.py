f17eb92ca06d6c6f1abf18c9f2d7c86aecaf45c8
chaudharyakshat239 <chaudharyakshat239@gmail.com>
2025-12-25 01:32:38 +0530

    "Dictionary_logic_building"

--- Day-6/Dictionary/Dictionary_logic_building.py
+++ Day-6/Dictionary/Dictionary_logic_building.py
@@ -1,77 +1,77 @@
 # Logic building questions
 
 # count the frequency of the array
-# a = [1,1,1,1,2,2,2,2,2,2,2,2,2,2,3,3,3,3,3,3,3,4,4,4,4,4,4,4,5,5,5,5,5,5]
-# d = {}
-# for i in a:
-#     if i in d.keys():
-#         d[i]+=1
-#     else:
-#         d[i] = 1 
+a = [1,1,1,1,2,2,2,2,2,2,2,2,2,2,3,3,3,3,3,3,3,4,4,4,4,4,4,4,5,5,5,5,5,5]
+d = {}
+for i in a:
+    if i in d.keys():
+        d[i]+=1
+    else:
+        d[i] = 1 
 
-# print(d)    
+print(d)    
 
 # check if two strings have same frequency map
-# a = "aabbcc"
-# b = "babacc"
+a = "aabbcc"
+b = "babacc"
 
-# if len(a)==len(b):
-#     d={}
-#     for i in a:
-#         if i in d.keys():
-#             d[i]+=1
-#         else:
-#             d[i]=1
-#     for i in b:
-#         if i in d.keys():
-#             d[i]-=1
-#         else:
-#             print("your elements are not same")
-#     for i in d:
-#         if d[i]!=0:
-#             print("not same elements")   
-#             break
-#     else:
-#         print("your strings are same")
-# else:
-#     print("not same strings")                         
+if len(a)==len(b):
+    d={}
+    for i in a:
+        if i in d.keys():
+            d[i]+=1
+        else:
+            d[i]=1
+    for i in b:
+        if i in d.keys():
+            d[i]-=1
+        else:
+            print("your elements are not same")
+    for i in d:
+        if d[i]!=0:
+            print("not same elements")   
+            break
+    else:
+        print("your strings are same")
+else:
+    print("not same strings")                         
  
         
 # Check if all characters of one string exist in another
 
-# a="akkubhaisahabh"
-# b="akkubhaisahabhjikijai"
-# d = {}
-# for i in a:
-#     if i in d.keys():
-#         d[i]+=1
-#     else:
-#         d[i]=1
-# for i in b:
-#     if i in d.keys():
-#         d[i]-=1
-#     else:
-#         print("String is not same") 
-# for i in d:
-#     if d[i]!=0:
-#         print("Not same string")    
-#         break
-#     else: 
-#         print("your string is same")          
+a="akkubhaisahabh"
+b="akkubhaisahabhjikijai"
+d = {}
+for i in a:
+    if i in d.keys():
+        d[i]+=1
+    else:
+        d[i]=1
+for i in b:
+    if i in d.keys():
+        d[i]-=1
+    else:
+        print("String is not same") 
+for i in d:
+    if d[i]!=0:
+        print("Not same string")    
+        break
+    else: 
+        print("your string is same")          
 
 
 # Find the unique element in a list
-# a=[1,1,1,1,1,2,2,2,2,23,3,3,3,3,4,4,4,4,5,5,5,5,]
-# d={}
-# for i in a:
-#     if i in d.keys():
-#         d[i]+=1
-#     else:
-#         d[i]=1
-# unqiue = 0  
-# for i in d:      
-#     if d[i] <= 1:
-#         print(i)
+a=[1,1,1,1,1,2,2,2,2,23,3,3,3,3,4,4,4,4,5,5,5,5,]
+d={}
+for i in a:
+    if i in d.keys():
+        d[i]+=1
+    else:
+        d[i]=1
+unqiue = 0  
+for i in d:      
+    if d[i] <= 1:
+        print(i)
 
 # Intersection of 2 Arrays
 a=[1,2,3,4,5,6]
