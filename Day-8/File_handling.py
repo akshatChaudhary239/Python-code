ec6723fbd51bd03059c5d7dfe81b9a169c80cd6b
chaudharyakshat239 <chaudharyakshat239@gmail.com>
2025-12-29 18:01:37 +0530

    "Day-8"

--- Day-8/File_handling.py
+++ Day-8/File_handling.py
@@ -10,3 +10,38 @@ except Exception as error:
     print(error)    
 
 print(a+b)    
+
+# else - we can use else with try and except too
+c = int(input("enter a number "))
+d = int(input("enter a number "))
+
+try:
+    print(c/d)
+except Exception as error:
+    print(error)  
+else:
+    print("correct numbers for division")      
+
+print(c+d)  
+
+# finally - it will execute no matter what
+
+e = int(input("enter a number "))
+f = int(input("enter a number "))
+try:
+    print(e/f)
+except Exception as e:
+    print(e)    
+finally:
+    print("i will be printed no matter what")    
+
+
+# Raise - we use to make custom errors warning
+
+age = int(input("enter eyour age "))
+try:
+    if age<18:
+        raise Exception("you must be 18+")
+    print("Access granted")
+except Exception as e:
+    print(e)        
