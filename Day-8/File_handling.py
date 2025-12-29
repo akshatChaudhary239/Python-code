d0a7aa6b22915ce5ef3a261bc32bc6c821aef2ea
chaudharyakshat239 <chaudharyakshat239@gmail.com>
2025-12-29 20:08:15 +0530

    "Day-8"

--- Day-8/File_handling.py
+++ Day-8/File_handling.py
@@ -1,47 +1,11 @@
-# exception handling
-# exceptions are raised when their is no ysntax error but the result still gets some error this doesn't stop the program but changes the flow of the program
+# we use this to perform crud operations 
+# we use open() to define any file name or define any path
+# to read - file_name.read()  
+#to write - open("file_name",'w')
+# to append -  open("file_name",'a')
+# to create a file we use 'x'
 
-a = int(input("enter a number "))
-b = int(input("enter a number "))
-
-try:
-    print(a/b)
-except Exception as error:
-    print(error)    
-
-print(a+b)    
-
-# else - we can use else with try and except too
-c = int(input("enter a number "))
-d = int(input("enter a number "))
-
-try:
-    print(c/d)
-except Exception as error:
-    print(error)  
-else:
-    print("correct numbers for division")      
-
-print(c+d)  
-
-# finally - it will execute no matter what
-
-e = int(input("enter a number "))
-f = int(input("enter a number "))
-try:
-    print(e/f)
-except Exception as e:
-    print(e)    
-finally:
-    print("i will be printed no matter what")    
-
-
-# Raise - we use to make custom errors warning
-
-age = int(input("enter eyour age "))
-try:
-    if age<18:
-        raise Exception("you must be 18+")
-    print("Access granted")
-except Exception as e:
-    print(e)        
+# using 'with'
+# with - it helps us to close the file automatically
+with open("file_name",'r') as fs:
+    print(file_name.read())
