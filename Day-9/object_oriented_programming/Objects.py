3fc3e8ef6aca30984e66836f703ce8ca56453005
chaudharyakshat239 <chaudharyakshat239@gmail.com>
2025-12-31 18:48:41 +0530

    "Day-9 objects"

---
+++ Day-9/object_oriented_programming/Objects.py
@@ -0,0 +1,10 @@
+# Objects -  we use the blueprint of classes to create object
+# we can call a class in a  variable to which we call a object
+# An object is powerful becuase it can access each and every attribute of the class
+class Person:
+    name = "Akku"
+    age = 20
+    work = "data scientist"
+me = Person()    
+print(me.name)
+print(me.agw)
